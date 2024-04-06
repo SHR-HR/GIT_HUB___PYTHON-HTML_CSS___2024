ff2373c65b2214a35777f903c93c48db801a2929
ᛃᛟᚹᚤᛈᛠᛋ, ᚢᚹᛋᛊᚷᚤᚳᛋ <150041912+SHR-HR@users.noreply.github.com>
2024-04-06 22:54:11 +0500

    LESSON_06_05.04.2024
    Практическая работа
    
    Курс: Разработка Web приложений на Python, с применением фреймворка Django
    Дисциплина: Разработка клиентских сценариев с использованием JavaScript
    
    Практическая работа №6: События, DOM.
    
    В заданиях дизайн можете опустить. Главное, чтобы была правильная логика.
    
    Выполните следующие задания:
    # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
    
    Задание 1.
    # ~ ~ ~ ~ ~
    Создать html-страницу со вкладками.
    С левой стороны страницы отображается несколько вкладок, по которым можно переключаться.
    У каждой вкладки есть свое содержимое, но в один момент времени отображается содержимое только активной вкладки.
    
    Задание 2.
    # ~ ~ ~ ~ ~
    Создать html-страницу с кнопкой Like, при нажатии на которую увеличивается счетчик лайков.
    
    Задание 3.
    # ~ ~ ~ ~ ~
    Создать html-страницу "Калькулятор". Реализовать его функциональность.
    
    # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ #
    
    Урок от 05.04.2024
    
    # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ #
    
    
    
    
    Домашняя работа
    
    Курс: Разработка Web приложений на Python, с применением фреймворка Django
    Дисциплина: Разработка клиентских сценариев с использованием JavaScript
    
    Домашнее задание (домашняя работа) №6: События, DOM.
    
    Выполните следующие задания:
    # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
    
    Задание 1.
    # ~ ~ ~ ~ ~
    Создать html-страницу со светофором и кнопкой, которая переключает светофор на следующий цвет.
    
    # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ #
    
    Урок от 05.04.2024
    
    # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ #

---
+++ LESSON_06_05.04.2024/index.js
@@ -0,0 +1,15 @@
+
+let js_p_two = document.getElementById('p_two')
+
+
+
+function onDocLoaded () {
+    setTimeout( function () {
+        let js_p_one = document.getElementById('p_one')
+        js_p_one.innerText = 'document is loaded'
+    
+    } , 300)   
+}
+
+
+document.addEventListener('DOMContentLoaded', )
